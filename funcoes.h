aa5581396b5a0338cb0512cc66fc0d863f728841
Rafael Hideaki Yara <79146870+rhy2106@users.noreply.github.com>
2024-11-18 00:23:37 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -12,11 +12,9 @@ typedef struct {
   char dia[11];
   char hora[6];
   char acao[2];
+  char dinheiro_nome[10][10];
+  float dinheiro[10];
   float valor;
-  float reais;
-  float btc;
-  float eth;
-  float xrp;
   float ct;
   float tx;
 } Extrato;
@@ -24,13 +22,10 @@ typedef struct {
 typedef struct {
   Extrato extratos[100];
   char nome[100];
+  char cpf[11];
   int senha;
   int cont; // quantidade de extratos
-  long int cpf;
-  float reais;
-  float btc;
-  float eth;
-  float xrp;
+  float dinheiro[10];
 } Pessoa;
 
 typedef struct {
@@ -40,22 +35,39 @@ typedef struct {
   float txv;
 } Moeda;
 
+// funcoes
 void limparString(char *p, int tam);
 void subString(char *tmp, char *linha, int comeco, int fim);
 int ContaCpfC(char *linha, Pessoa *contas, int ic);
-void adicionarExtrato(Pessoa *contas, Moeda moeda, char *acao, float valor,int id, int ic);
+
+// leitura / salvar arquivo
 void buildP(char *linha, Pessoa *l);
 void buildE(char *linha, Pessoa *p);
 void buildM(char *linha, Moeda *m);
-void adicionarExtrato(Pessoa *contas, Moeda moeda, char *acao, float valor, int id, int ic);
-void comprar(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
+void salvar(Pessoa *contas, Moeda *moedas, int ic, int im);
+int ler(Pessoa *contas, Moeda *moedas, int *ic, int *im);
+
+// logar
 int logar(Pessoa *contas, int ic);
+
+// cliente
 int menu();
-void vender(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
-void extrato(Pessoa conta);
-void saldo(Pessoa conta);
-void depositar(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
+void saldo(Pessoa conta, Moeda *moedas, int im);
+void extrato(Pessoa conta, Moeda *moedas, int im);
+void adicionarExtrato(Pessoa *contas, Moeda *moedas, char *acao, float valor, int id, int im,int idm);
 void sacar(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
+void depositar(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
+void comprar(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
+void vender(Pessoa *contas, Moeda *moedas, int ic, int im, int id);
 void atualizar(Moeda *moedas,int im);
+
+// admin
+int menu_admin();
+void cadastrar_m(Moeda *moedas, int *im);
+void excluir_invest(Pessoa *contas, Moeda *moedas, int *ic, int im);
+void excluir_m(Pessoa *contas, Moeda *moedas, int ic, int *im);
+void extrato_adm(Pessoa *contas, Moeda *moedas, int ic, int im);
+void saldo_adm(Pessoa *contas, Moeda *moedas, int ic, int im);
+void cadastrar(Pessoa *contas, int *ic);
 
 #endif
