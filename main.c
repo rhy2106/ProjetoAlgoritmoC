8f7006f1336ea61c83d33077b194a578834ccdcb
Rafael Hideaki Yara <79146870+rhy2106@users.noreply.github.com>
2024-11-18 00:25:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,40 +9,59 @@ int main() {
   char lixo,fim;
   int ic = 0, id, im = 0, cont; // i = index
   // ic = i conta, im = i moeda, ie = i extrato
-  FILE *arquivo; // arquivo
-  arquivo = fopen("contas.bin", "rb"); // le o arquivo de contas
-  while (fread(&contas[ic],sizeof(Pessoa),1,arquivo)) {
-    ic++;
-  }                // ic = quantidade de contas + 1
-  fclose(arquivo); // fecha o arquivo de contas
-
-  arquivo = fopen("moedas.bin", "rb"); // le o arquivo de moedas
-  while (fread(&moedas[im],sizeof(Moeda),1,arquivo)) {
-    im++;
-  }                // im = quantidade de moedas + 1
-  fclose(arquivo); // fecha o arquivo de moedas
+  
+  if(ler(contas,moedas,&ic,&im)) return 0;
+  int i = 0;
   while (1) {
-    id = logar(contas, ic - 1);
-    while (1) {
-      cont = menu();
-      if (cont == 1)
-        saldo(contas[id]); // M
-      else if (cont == 2)
-        extrato(contas[id]); // R
-      else if (cont == 3)
-        depositar(contas, moedas, ic, im, id); // M
-      else if (cont == 4)
-        sacar(contas, moedas, ic, im, id); // M
-      else if (cont == 5)
-        comprar(contas, moedas, ic, im, id); // R
-      else if (cont == 6)
-        vender(contas, moedas, ic, im, id); // R
-      else if (cont == 7)
-        atualizar(moedas, im); // M
-      else if (cont == 8)
-        break;
-      else
-        printf("opção invalida\n");
+    id = logar(contas, ic);
+    printf("%d",id);
+    if(id == 0){
+      while (1) {
+        salvar(contas, moedas, ic, im);
+        cont = menu_admin();
+        if (cont == 1)
+          cadastrar(contas, &ic);
+        else if (cont == 2)
+          excluir_invest(contas, moedas, &ic, im);
+        else if (cont == 3)
+          cadastrar_m(moedas, &im);
+        else if (cont == 4)
+          excluir_m(contas, moedas, ic, &im);
+        else if (cont == 5)
+          saldo_adm(contas,moedas,ic,im);
+        else if (cont == 6)
+          extrato_adm(contas, moedas, ic, im);
+        else if (cont == 7)
+          atualizar(moedas, im); // M
+        else if (cont == 8)
+          break;
+        else
+          printf("opção invalida\n");
+      }
+    }
+    else{
+      while (1) {
+        salvar(contas, moedas, ic, im);
+        cont = menu();
+        if (cont == 1)
+          saldo(contas[id], moedas, im); // M
+        else if (cont == 2)
+          extrato(contas[id], moedas, im); // R
+        else if (cont == 3)
+          depositar(contas, moedas, ic, im, id); // M
+        else if (cont == 4)
+          sacar(contas, moedas, ic, im, id); // M
+        else if (cont == 5)
+          comprar(contas, moedas, ic, im, id); // R
+        else if (cont == 6)
+          vender(contas, moedas, ic, im, id); // R
+        else if (cont == 7)
+          atualizar(moedas, im); // M
+        else if (cont == 8)
+          break;
+        else
+          printf("opção invalida\n");
+      }
     }
     while (1) {
       printf("Deseja finalizar o programa? (S/N)\n");
